1e295028c5d36463b5c3ac55591ab8568bef0ade
dec0deit <piyushsingla2416@gmail.com>
2022-02-28 18:23:10 +0530

    added

---
+++ 847.shortest-path-visiting-all-nodes.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=847 lang=cpp
+ *
+ * [847] Shortest Path Visiting All Nodes
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int shortestPathLength(vector<vector<int>>& graph) {
+        return 0;
+    }
+};
+// @lc code=end
+
