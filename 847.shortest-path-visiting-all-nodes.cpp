b6b3413797b03177e501c11e33ff4f54903dbe67
dec0deit <piyushsingla2416@gmail.com>
2022-02-28 19:22:34 +0530

    added

--- 847.shortest-path-visiting-all-nodes.cpp
+++ 847.shortest-path-visiting-all-nodes.cpp
@@ -7,8 +7,32 @@
 // @lc code=start
 class Solution {
 public:
-    int shortestPathLength(vector<vector<int>>& graph) {
-        return 0;
+    int shortestPathLength(vector<vector<int>>& g) {
+        long long n = g.size();
+        long long result = 0;
+        queue<vector<long long>> q;
+        vector<vector<long long>> visited(n,vector<long long>(1<<n));
+        for(int i=0;i<n;i++){
+            q.push({i,1<<i,0});
+            visited[i][1<<i] = true;
+        }
+        while (!q.empty())
+        {
+            vector<long long> temp= q.front();
+            q.pop();
+            if(temp[1]==(1<<n)-1){
+                result = temp[2];
+                return result;  
+            }
+            for(auto i:g[temp[0]]){
+                if(visited[i][(temp[1]|1<<i)]){
+                    continue;
+                }
+                q.push({i,(temp[1]|1<<i),temp[2]+1});
+                visited[i][(temp[1]|1<<i)] = true;
+            }
+        }
+        return result;
     }
 };
 // @lc code=end
